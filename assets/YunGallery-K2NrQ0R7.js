979272c4aed05a2f824a9c25e9052c2a504ec47c
Heyan-Lanan <Heyan-Lanan@users.noreply.github.com>
2024-01-09 09:52:33 +0000

    deploy: 40f7abd9ce877dfbcc1e7d7c778bf53b8d5e6df2

---
+++ assets/YunGallery-K2NrQ0R7.js
@@ -0,0 +1,1 @@
+import{_ as o}from"./gallery-WND-c3ap.js";import"./app--irjAYJF.js";import"./YunPageHeader.vue_vue_type_script_setup_true_lang-O2Yv1wdG.js";export{o as default};
---
+++ assets/_...all_-MxnUGJ9j.js
@@ -0,0 +1,1 @@
+import{f as a,a5 as s,u as n,a8 as r,o as c,h as p}from"./app--irjAYJF.js";import{b as t}from"./route-block-zudrvJp0.js";const u=a({__name:"[...all]",setup(f){const o=s(),e=n();return r(()=>{o.push("/"),e.path!=="/"&&e.path.endsWith("/")&&o.replace(e.path.slice(0,-1))}),(i,l)=>(c(),p("div"))}});typeof t=="function"&&t(u);export{u as default};
